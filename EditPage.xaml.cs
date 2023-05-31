5edc058e24ba6862b8f5108fbdb9c9b81e8eace1
Genoc1de <Wavesshark@yandex.ru>
2023-05-31 19:00:09 +0500

    Добавьте файлы проекта.

---
+++ EditPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOO_Knizhnii_Klub
+{
+    /// <summary>
+    /// Логика взаимодействия для EditPage.xaml
+    /// </summary>
+    public partial class EditPage : Page
+    {
+        private Product _currentProduct = new Product();
+        public EditPage(Product selectedProduct)
+        {
+            InitializeComponent();
+
+            if (selectedProduct != null)
+                _currentProduct = selectedProduct;
+
+            DataContext = _currentProduct;
+
+            ComboManufacturer.ItemsSource = DE3Entities.GetContext().Manufacturer.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentProduct.Name))
+                errors.AppendLine("Укажите название продукта");
+            if (string.IsNullOrWhiteSpace(_currentProduct.Description))
+                errors.AppendLine("Добавьте описание продукта");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentProduct.ID == 0)
+                DE3Entities.GetContext().Product.Add(_currentProduct);
+
+            try
+            {
+                DE3Entities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
