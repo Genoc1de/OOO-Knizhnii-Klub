5b5acf937220695bfb994b6c02667f37cbaea028
Genoc1de <Wavesshark@yandex.ru>
2023-06-06 20:01:53 +0500

    1

---
+++ Pages/OrderView.xaml.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace OOO_Knizhnii_Klub.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для OrderView.xaml
+    /// </summary>
+    public partial class OrderView : Page
+    {
+
+        private DE3Entities _context = new DE3Entities();
+        Order order;
+        List<ProductsInOrder> productsInOrder;
+
+
+        decimal allProductPrice;
+        decimal allProductAmount;
+        public OrderView(List<Product> products)
+        {
+            InitializeComponent();
+
+            order = new Order();
+            order.ID = DE3Entities.GetContext().Order.Max(o => o.ID) + 1;
+            productsInOrder = new List<ProductsInOrder>();
+            PickUpPointCB.ItemsSource = DE3Entities.GetContext().PickUpPoint.ToList();
+
+            foreach (var product in products)
+            {
+                var existingProductInOrder = productsInOrder.FirstOrDefault(p => p.ProductID == product.ID);
+                if (existingProductInOrder != null)
+                {
+                    // Увеличиваем количество товара, если он уже есть в корзине
+                    existingProductInOrder.QuantityProducts++;
+                }
+                else
+                {
+                    var productInOrder = new ProductsInOrder()
+                    {
+                        OrderID = order.ID,
+                        Product = product,
+                        ProductID = product.ID,
+                        QuantityProducts = 1
+                    };
+                    productsInOrder.Add(productInOrder);
+                }
+            }
+
+            OrderList.ItemsSource = productsInOrder;
+            CalculShowPriceAmount(productsInOrder);
+        }
+
+        private void CalculShowPriceAmount(List<ProductsInOrder> productList)
+        {
+            foreach (var product in productList)
+            {
+                allProductPrice += product.Product.Price * product.QuantityProducts;
+                if (product.Product.Discount != null)
+                {
+                    allProductPrice += Convert.ToDecimal(product.Product.Price - ((product.Product.Price / 100) * product.Product.Discount));
+                    allProductAmount += Convert.ToDecimal(product.Product.Discount);
+                }
+            }
+            ProductAmountTB.Text = $"{allProductAmount.ToString()}%";
+            ProductPriceTB.Text = $"{allProductPrice.ToString()}";
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button).DataContext is ProductsInOrder orderProduct)
+            {
+                var productInOrder = productsInOrder.Find(x => x.ProductID == orderProduct.ProductID);
+                if (productInOrder != null)
+                {
+                    productInOrder.QuantityProducts++;
+                }
+            }
+            CalculShowPriceAmount(productsInOrder);
+            OrderList.ItemsSource = productsInOrder;
+            OrderList.Items.Refresh();
+        }
+
+        private void minus_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button).DataContext is ProductsInOrder orderProduct)
+            {
+                productsInOrder.Where(x => x.ProductID == orderProduct.ProductID).FirstOrDefault().QuantityProducts--;
+                if (productsInOrder.Where(x => x.ProductID == orderProduct.ProductID).FirstOrDefault().QuantityProducts == 0)
+                {
+                    productsInOrder.Remove(productsInOrder.Where(x => x.ProductID == orderProduct.ProductID).FirstOrDefault());
+                }
+            }
+            CalculShowPriceAmount(productsInOrder);
+            OrderList.ItemsSource = productsInOrder;
+            OrderList.Items.Refresh();
+        }
+        private void FormOrder_Click(object sender, RoutedEventArgs e)
+        {
+            if (PickUpPointCB.SelectedItem != null)
+            {
+                var maxOrderCode = DE3Entities.GetContext().Order.Max(o => o.PickUpCode);
+                var newOrder = new Order()
+                {
+                    DeliveryDate = DeliveryDate(productsInOrder),
+                    PickUpPointID = ((PickUpPoint)PickUpPointCB.SelectedItem).ID,
+                    OrderStatusID = 0,
+                    PickUpCode = maxOrderCode++,
+                };
+                DE3Entities.GetContext().Order.Add(newOrder);
+                foreach (var item in productsInOrder)
+                {
+                    var newOrderProduct = new ProductsInOrder()
+                    {
+                        OrderID = newOrder.ID,
+                        ProductID = item.ProductID,
+                        QuantityProducts = item.QuantityProducts
+                    };
+                    DE3Entities.GetContext().ProductsInOrder.Add(newOrderProduct);
+                }
+                DE3Entities.GetContext().SaveChanges();
+                CreateTalon(newOrder, productsInOrder);
+
+            }
+            MessageBox.Show("Заказ оформлен");
+        }
+        private DateTime DeliveryDate(List<ProductsInOrder> oProductList)
+        {
+            if (oProductList.Count >= 6)
+            {
+                return DateTime.Now.AddDays(6);
+            }
+            else
+            {
+                foreach (var item in oProductList)
+                {
+                    if (item.Product.ProductQuantity < item.QuantityProducts)
+                    {
+                        return DateTime.Now.AddDays(6);
+                    }
+                }
+            }
+            return DateTime.Now.AddDays(3);
+
+        }
+        private void CreateTalon(Order order, List<ProductsInOrder> productsInOrders)
+        {
+            // Создание документа PDF
+            iTextSharp.text.Document doc = new iTextSharp.text.Document();
+            string filePath = "C:/Users/Rocket/Downloads/OrderReceipt.pdf"; // Путь для сохранения файла
+            iTextSharp.text.pdf.PdfWriter writer = iTextSharp.text.pdf.PdfWriter.GetInstance(doc, new FileStream(filePath, FileMode.Create));
+
+            doc.Open();
+
+            // Добавление заголовка
+            iTextSharp.text.Paragraph title = new iTextSharp.text.Paragraph("Талон заказа");
+            title.Alignment = iTextSharp.text.Element.ALIGN_CENTER;
+            doc.Add(title);
+
+            // Создание таблицы для отображения информации о заказе
+            PdfPTable table = new PdfPTable(2); // 2 столбца
+
+            // Добавление информации о заказе в таблицу
+            table.AddCell("Номер заказа");
+            table.AddCell(order.ID.ToString());
+
+            table.AddCell("Список продуктов");
+            foreach (var item in productsInOrders)
+            {
+                table.AddCell(item.Product.Name);
+            }
+
+            table.AddCell("Цена");
+            table.AddCell(allProductPrice.ToString());
+
+            table.AddCell("Скидка");
+            table.AddCell(allProductAmount.ToString());
+
+            table.AddCell("Пункт выдачи");
+            table.AddCell(DE3Entities.GetContext().PickUpPoint.Where(x => x.ID == order.PickUpPointID).FirstOrDefault().Name);
+
+            table.AddCell("Код получения");
+            table.AddCell(order.PickUpCode.ToString());
+
+            // Добавление таблицы в документ
+            doc.Add(table);
+
+            doc.Close();
+
+            MessageBox.Show("Талон заказа сохранен в файл " + filePath + ".");
+        }
+    }
+}
--- Pages/ProductPage.xaml.cs
+++ Pages/ProductPage.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace OOO_Knizhnii_Klub
 {
@@ -21,243 +20,75 @@ namespace OOO_Knizhnii_Klub
     /// </summary>
     public partial class ProductPage : Page
     {
-        private DE3Entities _context = new DE3Entities();
-        private Order CurrentOrder { get; set; } = new();
-        private List<ProductsInOrder> ProductsInOrder { get; set; } = new();
+        List<Product> products;
+        List<ProductsInOrder> productsInOrder;
+        Order order;
 
-
+        public Visibility ShowOrderVisivle
+        {
+            get
+            {
+                if (products != null || products.Count != 0)
+                {
+                    return Visibility.Visible;
+                }
+                return Visibility.Hidden;
+            }
+            set { }
+        }
 
         public ProductPage()
         {
             InitializeComponent();
             ListProducts.ItemsSource = DE3Entities.GetContext().Product.ToList();
 
-            ListOrderProducts.ItemsSource = DE3Entities.GetContext().ProductsInOrder.ToList();
-
-            ComboBoxOrderStatus.ItemsSource = _context.OrderStatus.ToList();
-            ComboBoxOrderStatus.SelectedItem = ((ComboBoxOrderStatus.ItemsSource as List<OrderStatus>)!).FirstOrDefault();
-            ComboBoxPickupPoint.ItemsSource = _context.PickUpPoint.ToList();
-
-
-        }
-
-        private void BtnAdd_Click(object sender, RoutedEventArgs e)
-        {
-            Manager.MainFrame.Navigate(new EditPage(null));
-        }
-
-        private void BtnDel_Click(object sender, RoutedEventArgs e)
-        {
-            var productsForRemoving = ListProducts.SelectedItems.Cast<Product>().ToList();
-
-            if (MessageBox.Show($"Вы точно хотите удалить следующие {productsForRemoving.Count()} элементов?", "Внимание",
-                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
-            {
-                try
-                {
-                    DE3Entities.GetContext().Product.RemoveRange(productsForRemoving);
-                    DE3Entities.GetContext().SaveChanges();
-                    MessageBox.Show("Данные удалены");
-
-                    ListProducts.ItemsSource = DE3Entities.GetContext().Product.ToList();
-
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message.ToString());
-                }
-            }
-
-        }
-
-        private void BtnEdit_Click(object sender, RoutedEventArgs e)
-        {
-
-            Manager.MainFrame.Navigate(new EditPage((sender as Button).DataContext as Product));
         }
 
         private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
+
         }
 
-        private void MainTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void AddInOrder_Click(object sender, RoutedEventArgs e)
         {
-            switch ((((TabControl)sender).SelectedItem as TabItem)?.Name)
+            if (products == null)
             {
-                case "TabItemListProducts":
-                    ButtonCreateOrder.Visibility = Visibility.Hidden;
-                    break;
-                case "TabItemOrder":
-                    ButtonCreateOrder.Visibility = Visibility.Visible;
-                    break;
+                products = new List<Product>();
             }
-        }
-
-        private void DeleteOrderProduct_Click(object sender, RoutedEventArgs e)
-        {
-            ProductsInOrder.Remove((((Button)sender).DataContext as ProductsInOrder)!);
-
-            ListOrderProducts.ItemsSource = new List<ProductsInOrder>();
-            ListOrderProducts.ItemsSource = ProductsInOrder;
-        }
-
-        private void GridProducts_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            if ((sender as MenuItem).DataContext is Product product)
             {
+                products.Add(product);
             }
-            else if (e.RightButton == MouseButtonState.Pressed)
-            {
-                var result = MessageBox.Show("Add a product to an order?", "Adding a product to the cart", MessageBoxButton.YesNo);
-                switch (result)
-                {
-                    case MessageBoxResult.Yes:
-                        AddOrderProduct(sender);
-                        if (TabItemOrder.IsEnabled == false)
-                        {
-                            TabItemOrder.IsEnabled = true;
-                        }
-                        break;
-                    case MessageBoxResult.Cancel:
-                        break;
-                    case MessageBoxResult.None:
-                    case MessageBoxResult.OK:
-                    case MessageBoxResult.No:
-                    default:
-                        break;
-                }
+            MessageBox.Show("Продукт добавлен в корзину.");
 
-                ListOrderProducts.ItemsSource = new List<ProductsInOrder>();
-                ListOrderProducts.ItemsSource = ProductsInOrder;
-            }
+            ShowOrder.Visibility = Visibility.Visible;
         }
 
-        private void AddOrderProduct(object sender)
+
+
+        private void ShowOrder_Click(object sender, RoutedEventArgs e)
         {
-            foreach (var p1 in ProductsInOrder.Where(p1 => p1.Product.ID == (((Product)((Grid)sender).DataContext)!).ID))
-            {
-                p1.QuantityProducts++;
-                return;
-            }
-
-            ProductsInOrder.Add(new ProductsInOrder()
-            {
-                Order = CurrentOrder,
-                QuantityProducts = 1,
-                Product = (Product)((Grid)sender).DataContext ?? new Product(),
-                OrderID = CurrentOrder.ID,
-                ProductID = (((Product)((Grid)sender).DataContext)!).ID
-            });
+            Pages.OrderView orderView = new Pages.OrderView(products);
+            Manager.MainFrame.Navigate(new Pages.OrderView(products));
         }
 
-        private void ButtonCreateOrder_Click(object sender, RoutedEventArgs e)
+        private void BtnAddInOrder_Click(object sender, RoutedEventArgs e)
         {
-            switch (TabItemMakingOrder.IsEnabled)
+            if (products == null)
             {
-                case true:
-                    TabItemMakingOrder.IsEnabled = false;
-                    ButtonCreateOrder.Content = "CREATE ORDER";
-                    break;
-                case false:
-                    TabItemMakingOrder.IsEnabled = true;
-                    ButtonCreateOrder.Content = "ORDER CANCELLATION";
-                    break;
+                products = new List<Product>();
             }
+            Product selectedProduct = (Product)ListProducts.SelectedItem;
+            products.Add(selectedProduct);
+            MessageBox.Show("Продукт добавлен в корзину.");
+
+            ShowOrder.Visibility = Visibility.Visible;
+
         }
-
-
-        private async void ButtonSendOrder_Click(object sender, RoutedEventArgs e)
-        {
-            var maxGetCode = _context.Order.Max(o => o.PickUpCode);
-
-            try
-            {
-                await AddOrder(maxGetCode);
-                MessageBox.Show("Success");
-                await PrintOrderCard();
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show(exception.Message);
-            }
-
-            RefreshWindow();
-        }
-
-        private async Task AddOrder(int maxGetCode)
-        {
-            using (var context = new DE3Entities())
-            {
-                this.CurrentOrder = new Order()
-                {
-                    PickUpPointID = ((PickUpPoint)ComboBoxPickupPoint.SelectedItem).ID,
-                    OrderStatusID = ((OrderStatus)ComboBoxOrderStatus.SelectedItem).ID,
-                    DeliveryDate = DateTime.Now,
-                    PickUpCode = maxGetCode++
-                };
-
-                DE3Entities.GetContext().Order.Add(CurrentOrder);
-                await context.SaveChangesAsync();
-
-                this.CurrentOrder.ProductsInOrder = this.ProductsInOrder;
-
-            }
-        }
-        private Task PrintOrderCard()
-        {
-            var orders = CurrentOrder;
-            var app = new Excel.Application
-            {
-                SheetsInNewWorkbook = 1
-            };
-
-            var workbook = app.Workbooks.Add(Type.Missing);
-
-            Excel.Worksheet worksheet = app.Worksheets.Item[1];
-            worksheet.Name = "Card";
-
-            worksheet.Cells[1][1] = "Order number";
-            worksheet.Cells[1][2] = "Product list";
-            worksheet.Cells[1][3] = "Total cost";
-
-            worksheet.Cells[2][1] = orders.ID;
-
-            var fullProductList = string.Empty;
-            fullProductList = orders.ProductsInOrder.Aggregate(fullProductList,
-                (current, product) => current + $"{product.Product.Name}\n");
-            worksheet.Cells[2][2] = fullProductList;
-            worksheet.Cells[2][3] = orders.ProductsInOrder.Sum(p => p.Product.Price);
-
-            worksheet.Columns.AutoFit();
-
-            app.Visible = true;
-
-            app.Application.ActiveWorkbook.SaveAs(@"C:\Users\Rocket\source\repos\OOO Knizhnii Klub\test.xlsx"); 
-
-            var excelDocument = app.Workbooks.Open(@"C:\Users\Rocket\source\repos\OOO Knizhnii Klub\test.xlsx");
-
-            excelDocument.ExportAsFixedFormat(Excel.XlFixedFormatType.xlTypePDF, @"C:\Users\Rocket\source\repos\OOO Knizhnii Klub\test.pdf");
-            excelDocument.Close(false, "", false);
-            app.Quit();
-            return Task.CompletedTask;
-        }
-
-        private void RefreshWindow()
-        {
-            this.CurrentOrder = new Order();
-            this.ProductsInOrder = new List<ProductsInOrder>();
-
-            ListOrderProducts.ItemsSource = new List<ProductsInOrder>();
-            ListOrderProducts.ItemsSource = ProductsInOrder;
-
-            MainTabControl.SelectedIndex = 0;
-            TabItemOrder.IsSelected = false;
-            TabItemMakingOrder.IsEnabled = false;
-            ButtonCreateOrder.Content = "CREATE ORDER";
-            ButtonCreateOrder.Visibility = Visibility.Hidden;
-        }
-
     }
-    }
+}
+
+
+
 
 
